6b5f17cdca07a547156fa0683469ced75c99ae11
surajthaakur7 <158021859+surajthaakur7@users.noreply.github.com>
2026-01-09 12:37:16 +0530

    made chnages to cal.py

--- cal.py
+++ cal.py
@@ -18,3 +18,24 @@ elif operation == '/':
         print("Error: Division by zero is not allowed.")
 else:
     print("Invalid operation. Please enter a valid operation (+, -, *, /).")
+proceed = input("do you want to proceed : yes/no")
+if proceed=="yes":
+    value3 = int(input("Enter a number: "))
+    value4 = int(input("Enter another number: "))
+    operation = input("Enter operation (+, -, *, /): ")
+    if operation == '+':
+        print(f"Result after addition: {result}")
+    elif operation == '-':
+        result = value3 - value4
+        print(f"Result after subtraction: {result}")
+    elif operation == '*':
+        result = value3 * value4
+        print(f"Result after multiplication: {result}")
+    elif operation == '/':
+        if value4 != 0:
+            result = value3 / value4
+            print(f"Result after division: {result}")
+        else:
+            print("Error: Division by zero is not allowed.")
+    else:
+        print("Invalid operation. Please enter a valid operation (+, -, *, /).")
