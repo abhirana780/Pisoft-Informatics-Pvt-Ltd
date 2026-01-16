8bb1bc5be279cf8fb6a6ed4fa70cc5c8fc7a3fd7
Suraj Thakur <158021859+surajthaakur7@users.noreply.github.com>
2026-01-16 11:57:55 +0530

    Delete multiplication_table.py

--- multiplication_table.py
+++
@@ -1,5 +0,0 @@
-#multiplication table
-num = int(input("Enter a number to display its multiplication table: "))
-for i in range(1, 11):
-    result = num * i
-    print(f"{num} x {i} = {result}")   
